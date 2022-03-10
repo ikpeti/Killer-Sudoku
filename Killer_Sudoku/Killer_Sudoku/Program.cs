9d03e44003b039d240ccc3f303f59edb3407f656
ikpeti <79321145+ikpeti@users.noreply.github.com>
2022-03-10 15:14:03 +0100

    output(4x4)

---
+++ Killer_Sudoku/Killer_Sudoku/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Killer_Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size:");
+            SudokuBoard sb = new SudokuBoard(Convert.ToInt32(Console.ReadLine()));
+            sb.ReadInput();
+            sb.ShowBoard();
+        }
+    }
+}
---
+++ Killer_Sudoku/Killer_Sudoku/SudokuBoard.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Killer_Sudoku
+{
+    public class SudokuBoard
+    {
+        private int size;
+        private int[,] board;
+
+        public SudokuBoard(int s)
+        {
+            size = s;
+            board = new int[size, size];
+        }
+
+        public void ReadInput()
+        {
+            for (int i = 0; i<size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    board[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if(!CheckBoard())
+                        Console.WriteLine("Hiba");
+                }
+            }
+        }
+
+        public void ShowBoard()
+        {
+            //4*4
+            for (int i = 0; i < size; i++)
+            {
+                if(i == size/2)
+                {
+                    Console.WriteLine("---------");
+                }
+                for (int j = 0; j < size; j++)
+                {
+                    if(j == size/2)
+                    {
+                        Console.Write("| ");
+                    }
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool CheckBoard()
+        {
+            bool[] check = new bool[size];
+            
+            for (int j = 0; j < size; j++)
+            {
+                check[j] = false;
+            }
+
+            for (int i = 0; i<size; i++)
+            {
+                for(int j = 0;j<size;j++)
+                {
+                    if (board[i, j] != 0)
+                    {
+                        if (check[board[i, j] - 1]) return false;
+                        else check[board[i, j] - 1] = true;
+                    }
+                }
+            
+                for (int j = 0; j < size; j++)
+                {
+                    check[j] = false;
+                }
+            }
+
+            return true;
+        }
+        public int GetSize()
+        {
+            return size;
+        }
+    }
+}
